3e7c9e82dac9be9077257823cc36018e13d5b92a
Kawan Lucas Matsue dos Santos <128930959+Fainalspaice@users.noreply.github.com>
2024-08-05 23:55:15 -0300

    Add files via upload

---
+++ jogoAdivinhação.java
@@ -0,0 +1,10 @@
+package projetinhos;
+
+import java.util.Random;
+
+
+public class jogoAdivinhação {
+    public static void main (String[] args) {
+        Random random = new Random(56);
+    }
+}
